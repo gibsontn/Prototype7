6827f1eff4fad3ad3db1fd9ace4e17303019aa80
T Gibson <taylornicolegibson@gmail.com>
2022-10-11 12:18:03 -0400

    scenes scripted, working

--- Weather/Assets/Scripts/Win.cs
+++ Weather/Assets/Scripts/Win.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class End : MonoBehaviour
+public class Win : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
